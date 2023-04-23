b49f1f6f8f3889d7e553c2a9198edb1bc43d8411
veronicacheung <veronica.cheung1105@gmai.com>
2023-04-23 12:30:26 -0700

    Initial Commit

---
+++ aboutRoniApp/aboutRoniAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutRoniAppApp.swift
+//  aboutRoniApp
+//
+//  Created by Veronica Cheung on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutRoniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
